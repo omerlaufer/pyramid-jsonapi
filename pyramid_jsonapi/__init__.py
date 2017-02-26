b3b44744c2bf9955c3551ded366776e5b4b086b6
Amichay Oren <amichay@appcard.com>
2017-02-26 10:50:42 +0200

    remove import
    remote unused import

--- pyramid_jsonapi/__init__.py
+++ pyramid_jsonapi/__init__.py
@@ -36,7 +36,6 @@ from sqlalchemy.orm import load_only
 from sqlalchemy.exc import DBAPIError
 from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
 from sqlalchemy.orm.relationships import RelationshipProperty
-from sqlalchemy import or_
 from sqlalchemy.ext.declarative.api import DeclarativeMeta
 from sqlalchemy.ext.hybrid import hybrid_property
 
