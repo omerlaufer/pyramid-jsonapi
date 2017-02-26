ef7c51f632be081be7d7478c9d3693586232aa5c
Amichay Oren <amichay@appcard.com>
2017-02-26 10:53:07 +0200

    removing spaces

--- pyramid_jsonapi/__init__.py
+++ pyramid_jsonapi/__init__.py
@@ -1825,7 +1825,7 @@ class CollectionViewBase:
             q = q.options(load_only(rel_view.key_column.name))
         if rel.direction is ONETOMANY:
             q = q.filter(obj_id == rem_col)
-        elif rel.direction is MANYTOMANY:        
+        elif rel.direction is MANYTOMANY:
             q = q.filter(
                 obj_id == rel.primaryjoin.right
             ).filter(
